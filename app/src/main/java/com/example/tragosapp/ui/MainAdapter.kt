3b7850a964edd13ddee7ed3de10ef5b04ed1a233
brianA1998 <brian.alaniz@hotmail.com>
2021-10-13 22:41:49 -0300

    Implementando view binding en vez de synthetic que usa el video parte 3

--- app/src/main/java/com/example/tragosapp/ui/MainAdapter.kt
+++ app/src/main/java/com/example/tragosapp/ui/MainAdapter.kt
@@ -1,24 +1,44 @@
 package com.example.tragosapp.ui
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.tragosapp.R
 import com.example.tragosapp.base.BaseViewHolder
 import com.example.tragosapp.data.model.Drink
+import com.example.tragosapp.databinding.TragosRowBinding
 
 class MainAdapter(private val context: Context, private val tragosList: List<Drink>) :
     RecyclerView.Adapter<BaseViewHolder<*>>() {
-    
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        TODO("Not yet implemented")
+        val binding = TragosRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        TODO("Not yet implemented")
+        when (holder) {
+            is MainViewHolder -> holder.bind(tragosList[position], position)
+        }
     }
 
     override fun getItemCount(): Int {
         return tragosList.size
+    }
+
+    inner class MainViewHolder(val binding: TragosRowBinding) :
+        BaseViewHolder<Drink>(binding.root) {
+        override fun bind(item: Drink, position: Int) {
+
+
+            Glide.with(context).load(item.imagen).into(binding.imgTrago)
+            binding.txtTitulo.text = item.nombre
+            binding.txtDescripcion.text = item.descripcion
+        }
+
     }
 
 }
